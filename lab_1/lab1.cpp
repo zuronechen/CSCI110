e21583266ff28d2367f0045eba26ca0cc0290ce3
zuronechen <zuronechen@gmail.com>
2019-01-10 09:59:29 -0800

    second push

--- lab_1/lab1.cpp
+++ lab_1/lab1.cpp
@@ -9,7 +9,8 @@ int main()
 {
     float time_1;
     float time_2;
-    const float midnight = 2360;
+    const int minutes = 60;
+    const float midnight = 24*minutes;
     
     
 
@@ -18,16 +19,28 @@ int main()
     cout << "Please enter the second time: " << endl;
     cin >> time_2;
 
-    int total_1 = (time_2 - time_1);
-    int total_2 = (midnight - time_1) + time_2;
+    int firstcase_difference = (time_2 - time_1);
+    int firstcase_hours = firstcase_difference / 100;
+    int firstcase_minutes = (firstcase_difference % 100);
+
+    int secondcase_difference = (midnight - time_1) + time_2;
+    int secondcase_hours = secondcase_difference / 100;
+    int secondcase_minutes = (secondcase_difference % 100);
+    
+    if (secondcase_minutes >= 60)
+        secondcase_minutes - 60;
+        secondcase_hours = (secondcase_difference / 100) + 1;
 
     if (time_1 < time_2)
-        cout << "The total amount of time in between is: " << (total_1 / 100) << " hours" << " and " << (total_1 % 100) << " minutes" << endl;
-    else
-        cout << "The total amount of time in between is: " << (total_2 / 100) << " hours" << " and " << (total_2 % 100) << " minutes" << endl;
+    {
+        cout << "The total amount of time in between is: " << firstcase_hours << " hours" << " and " << firstcase_minutes << " minutes" << endl;
+    }
+    else 
+    {
+        cout << "The total amount of time in between is: " << secondcase_hours << " hours" << " and " << secondcase_minutes << " minutes" << endl;
+    }
     
     // system("pause"); 
     
     return 0;
 }
-
