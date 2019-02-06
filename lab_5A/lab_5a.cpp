d750ab5fb70b43d7bc31a66049efb396635f4670
zuronechen <zuronechen@gmail.com>
2019-02-05 18:38:09 -0800

    second push

--- lab_5A/lab_5a.cpp
+++ lab_5A/lab_5a.cpp
@@ -6,35 +6,79 @@
 #include <iomanip>
 #include <cstdlib>
 #include <ctime>
-#include<bits/stdc++.h>
+#include <algorithm>
 
 using namespace std;
 
+const int CAPACITY = 10;
 
-bool equals(int a[], int b[])
+bool equals(int a[], int a_size, int b[], int b_size)
 {
-    int i;
-    bool is_same = true; 
-
-    for (i = 0; i < 10; i++)
+    if (a_size == b_size)
     {
-        
-        if (a[i] != b[i])
+        for (int i = 0; i < a_size; i++) // once the arrays pass the length test, this will check for equality of values 
         {
-            is_same = false;
+            if (a[i] != b[i])
+            {
+                return false; 
+            }
         }
+        return true;
     }
-
-    return is_same;
+    else
+    {
+        return false; // if the array sizes are different, return false
+    }
 }
+
 
 int main() 
 {
-int array_1[];
-int array_2[];
-cout << "Please enter an array: " << endl;
-cin >> 
+    int first_array[20], second_array[20];
+    int first_size, second_size;
 
+    cout << "Please enter the first array size: "; // declare first array length
+    cin >> first_size; // store first array length
 
+    cout << "Please enter the first array: "; 
+    for (int i = 0; i < first_size; i++)
+    {
+        cin >> first_array[i]; // store each value of first array one by one
+    }
+
+    cout << "Please enter the second array size: "; // declare second array length
+    cin >> second_size; // store second array length
+
+    cout << "Please enter the second array: ";
+    for (int i = 0; i < second_size; i++)
+    {
+        cin >> second_array[i]; // store each value of second array one by one 
+    }
+    cout << endl;
+
+    cout << "first array: ";
+    for (int i = 0; i < first_size; i++)
+    {
+        cout << first_array[i] << " "; // display each value of first array separated by space
+    }
+    cout << endl;
+
+    cout << "second array: ";
+    for (int i = 0; i < second_size; i++)
+    {
+        cout << second_array[i] << " "; // display each value of second array separated by space
+    }
+    cout << endl;
+
+    if (equals(first_array, first_size, second_array, second_size)) // calling the bool function 
+    {
+        cout << "Both arrays are the same." << endl;
+    }
+    else
+    {
+        cout << "The two arrays are different." << endl;
+    }
+
+//system("pause");
 return 0;
-}
+}   
