f9efd09ff917d59dad0b90e841d7a95ff2751a83
zuronechen <zuronechen@gmail.com>
2019-02-10 23:37:49 -0800

    yay!

--- lab_6B/lab_6b.cpp
+++ lab_6B/lab_6b.cpp
@@ -1,9 +1,9 @@
 /* 
-Description: A program that reads a user input array and reverses the order of the values using pointers.
+Description: A program that reads a user input array until termination by Q, then allocates the array of correct size and copies all inputs into it.
 Author: Zurone Chen
-Input Variables: double a[] (original array)
-Process Flow: while reading the values of an array, the last position of the array gets swapped with the first position of the array.
-Output Variables: reversed array
+Input Variables: double *initial_array; double* new_array;
+Process Flow: while reading the values of an array, the size changes according to the number of inputted elements, then prints all elements.
+Output Variables: new_array
 */
 
 // #include "stdafx.h"
@@ -18,27 +18,67 @@ Output Variables: reversed array
 
 using namespace std;
 
-int* read_data(int& size)
+double* read_data(int& size)
 {
-    int* arr = new int[10];
-    int s = 0, i = 10;
-    char input[10]; 
-    cout << "Enter salaries, enter Q to quit: ";
-    bool more = true;
-    while (true)
+    int initial_size;
+    int temp;
+    double* initial_array = new double[10];
+
+    cout << "Please enter numbers for an array, Q to quit: "; // read data
+    for (int i = 0; i < 10; i++)
     {
-        cin >> input;
+        cin >> initial_array[i];
+        size++;
+    }
+    cout << "Array size: " << size << endl;
+    
+    if (size == 10)
+    {
+        temp = size;
+        initial_size = temp;
+        size = size * 2; // double array size when the array fills up with 10 values
+    }
+
+    double* new_array = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        new_array[i] = initial_array[i];
+    }
+    delete []initial_array; // delete first array once copied to the new array with double capacity
+
+    int new_size = 0;
+
+    cout << "Please enter more numbers for the array, Q to quit: "; // keep reading data
+    for (int i = initial_size; i < size; i++)
+    {
+        cin >> new_array[i];
         if (cin.fail())
         {
-            more = false; {break; }
+            new_size--;
+            size--;
         }
+        new_size++;
     }
-    cin.clear(); // reset if more input is to be taken
+    cout << "Array size: " << new_size << endl << endl;
 
+    size = initial_size + new_size;
+
+    return new_array;
 }
 
 int main()
 {
+    int size = 0;
+    double* array = read_data(size);
+
+    cout << "Final array: ";
+    for (int i = 0; i < size; i++)
+    {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+    cout << "Final array size: " << size << endl;
+    
 
     //system("pause")
     return 0;
